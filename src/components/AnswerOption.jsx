16eccd0a2de916e7f9aab63f0042c2e535791233
SamM922 <samuel.maturo@gmail.com>
2020-01-15 18:16:37 -0800

    Added answer files

---
+++ src/components/AnswerOption.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../css/AnswerOption.css';
+
+class AnswerOption extends Component {
+
+    render() {
+      return (
+        <div>
+            
+        </div>
+      );
+    }
+  }
+  
+  export default AnswerOption;
--- src/components/AnswersBox.jsx
+++ src/components/AnswersBox.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import '../css/AnswersBox.css';
 
 class AnswersBox extends Component {
   
     render() {
+        let answerOptions = [];
+        let answerOptions_ = [];
+        for(let i = 0; i > answerOptions.length; i++) {
+            answerOptions_.push(<AnswerOption title={answerOptions[i]}/>);
+        }
       return (
         <div>
             
