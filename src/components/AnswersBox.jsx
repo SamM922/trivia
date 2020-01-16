950959f45f6c9a8f5f87d1a23ae7717a89046e02
SamM922 <samuel.maturo@gmail.com>
2020-01-15 17:46:27 -0800

    Create AnswersBox

---
+++ src/components/AnswersBox.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AnswersBox extends Component {
+  
+    render() {
+      return (
+        <div>
+            
+        </div>
+      );
+    }
+  }
+  
+  export default AnswersBox;
