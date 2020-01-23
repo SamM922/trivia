151b8986c354f8c1913a13e3b1a93b8f7e395f14
SamM922 <samuel.maturo@gmail.com>
2020-01-22 17:55:38 -0800

    Changed AnswersBox mainly

--- src/components/AnswerOption.jsx
+++ src/components/AnswerOption.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import '../css/AnswerOption.css';
 
 class AnswerOption extends Component {
+    constructor(props) {
+        super(props);
+    }
 
     render() {
       return (
