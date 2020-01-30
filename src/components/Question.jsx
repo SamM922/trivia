710a60d66fb0c32aeaec7ac63ba4386da2b99e1e
SamM922 <samuel.maturo@gmail.com>
2020-01-29 18:09:38 -0800

    began using data for questions

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -8,21 +8,20 @@ class Question extends Component {
     super(props);
   }
   
+  constructor(data) {
+    const{choices, correct_choice_index, question_text, user_id} = data;
+    this.choices = choices;
+    this.correct_choice_index = correct_choice_index;
+    this.question_text = question_text;
+    this.user_id = user_id;
+  }
 
   handleClick() {
     var nextQuestion = this.state.nextQuestion + 1;
-    let questionList = ["In which film did Bill Murray play the character Dr. Peter Venkman?",
-  "On 'Legally Blonde', what is Warner's nickname for Elle?", 
-  "What was the name of Jack Black's character in the movie 'School of Rock'?"];
   }  
   
   render() {
-    let questionData = question;
-
-    for (let i = 0; i < questionData; i++){
-      
-    }
-
+    
     return (
       <div>
         <button onClick={()=> this.handleClick()}>Next</button>
