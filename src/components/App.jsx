c410d4faa78fb77d521791f20e9383986f683a65
SamM922 <samuel.maturo@gmail.com>
2020-01-29 18:13:00 -0800

    fixed question mixup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,13 @@ import Question from '../components/Question';
 // import components
 
 class App extends Component {
-
+  constructor(data) {
+    const{choices, correct_choice_index, question_text, user_id} = data;
+    this.choices = choices;
+    this.correct_choice_index = correct_choice_index;
+    this.question_text = question_text;
+    this.user_id = user_id;
+  }
   render() {
     return (
       <div className="app">
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -7,21 +7,13 @@ class Question extends Component {
   constructor(props) {
     super(props);
   }
-  
-  constructor(data) {
-    const{choices, correct_choice_index, question_text, user_id} = data;
-    this.choices = choices;
-    this.correct_choice_index = correct_choice_index;
-    this.question_text = question_text;
-    this.user_id = user_id;
-  }
 
   handleClick() {
     var nextQuestion = this.state.nextQuestion + 1;
   }  
   
   render() {
-    
+
     return (
       <div>
         <button onClick={()=> this.handleClick()}>Next</button>
