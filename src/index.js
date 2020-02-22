ddd142f5e1e4cf069e81d9046c96f2408a00120d
Santos Castaneda <santoscastaneda@Gs-iMac.local>
2020-02-21 16:58:25 -0800

    question update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/App.css';
-import App from './components/App';
-import {buildFirebase} from './clients/firebase.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/App.css";
+import App from "./components/App";
+import { buildFirebase } from "./clients/firebase.js";
 
-let questions = {}
+let questions = {};
+
 var database = buildFirebase();
 var databaseRef = database.ref("/questions");
 databaseRef.once("value").then(function(data) {
- const questions = Object.values(data.val());
- console.log(questions);
+  const questions = Object.values(data.val());
+  console.log(questions);
+  ReactDOM.render(<App data={questions} />, document.getElementById("root"));
 });
-ReactDOM.render(<App />, document.getElementById('root'));
